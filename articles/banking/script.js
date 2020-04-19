f6c48f630232004c197d7f8263fbd8c7365f6108
PevetsVictoria <49641778+PevetsVictoria@users.noreply.github.com>
2020-04-19 15:14:28 +0300

    19 april

---
+++ articles/banking/script.js
@@ -0,0 +1,294 @@
+
+
+(function (d, w) {
+        var  s = d.createElement('script'),
+            f = function() {document.body.appendChild(s);};
+        var container = 'partner_form_napishem';
+        var charset = document.characterSet ? document.characterSet : document.charset;
+        s.async = false;
+        s.src = 'https://partner.napishem.ru/widget.js?id=3129&c=' + container + '&charset=' + charset;
+        if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+        w.addEventListener('load', f);
+})(document, window);
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem1';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3130&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem2';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3133&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem3';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3134&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem4';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3152&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem5';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3137&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem6';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3138&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem7';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3139&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem8';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3141&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem9';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3143&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem10';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3144&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem11';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3146&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem12';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3147&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem13';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3148&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem14';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3149&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem15';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3153&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem16';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3154&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem17';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3156&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem18';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3159&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem19';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3160&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem20';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3162&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem21';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3164&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
+
+
+
+(function (d, w) {
+    var  s = d.createElement('script'),
+        f = function() {document.body.appendChild(s);};
+    var container = 'partner_form_napishem22';
+    var charset = document.characterSet ? document.characterSet : document.charset;
+    s.async = false;
+    s.src = 'https://partner.napishem.ru/widget.js?id=3165&c=' + container + '&charset=' + charset;
+    if (w.opera == '[object Opera]') return d.addEventListener('DOMContentLoaded', f, false);
+    w.addEventListener('load', f);
+})(document, window);
